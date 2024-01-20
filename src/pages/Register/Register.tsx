eaf3e8db5d84d2d1515c59edd11bc832e4d97010
hangduchuy <duchuy246938@gmail.com>
2024-01-20 17:08:02 +0700

    Validation với Yup form Login / Register,Cấu hình Axios, React Query

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,72 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import Input from 'src/components/Input'
+import { Schema, schema } from 'src/utils/rules'
+
+type FormData = Schema
+
 export default function Register() {
-  return <div>Register</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
+  return (
+    <div className='bg-orange'>
+      <div className='container'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
+              <div className='text-2xl'>Đăng ký</div>
+              <Input
+                name='email'
+                register={register}
+                type='email'
+                className='mt-8'
+                errorMessage={errors.email?.message}
+                placeholder='Email'
+              />
+              <Input
+                name='password'
+                register={register}
+                type='password'
+                className='mt-2'
+                errorMessage={errors.password?.message}
+                placeholder='Password'
+                autoComplete='on'
+              />
+              <Input
+                name='confirm_password'
+                register={register}
+                type='password'
+                className='mt-2'
+                errorMessage={errors.confirm_password?.message}
+                placeholder='Confirm password'
+                autoComplete='on'
+              />
+              <div className='mt-2'>
+                <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
+                  Đăng ký
+                </button>
+              </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-gray-300'>Bạn đã có tài khoản? </span>
+                <Link to={'/login'} className='text-red-400 ml-1'>
+                  Đăng nhập
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
